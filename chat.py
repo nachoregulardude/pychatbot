3eac3bfda9bbb94a9777bc70d598e27019a0393c
Bharath G <bharathg@pop-os.localdomain>
2022-03-20 23:30:49 +0530

    Imporved the responses, made it more personalized. Gave it some coconut oil for the smoothness

--- chat.py
+++ chat.py
@@ -1,9 +1,18 @@
 import random
+import re
 import json
 import torch
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 
+def refactor_response(response):
+    if (re.search(pattern, response)):
+        res = re.findall(pattern, response)
+        for var in res:
+            #a smart/jugaad to turn a string to a variable name I is proud of this :)
+            want = globals()[(var.replace('{', '').replace('}', ''))]
+            response = response.replace(var, want)
+    return response
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -27,6 +36,14 @@ model.eval()
 bot_name = "SmartBoi"
 print(f"Start chatting with {bot_name}! (type 'quit' to exit)")
 falseCounter = 0
+name = input("Please enter your name: ")
+call = '8073982643'
+link = 'reflecton.in'
+location = 'g.maps'
+locationlink = 'g.maps'
+appointmentlink = 'appoint.link'
+pattern = r"{[a-z]+\}"
+want = ''
 while falseCounter<3:
     # sentence = "do you use credit cards?"
     sentence = input("You: ")
@@ -51,8 +68,18 @@ while falseCounter<3:
     prob = probs[0][predicted.item()]
     if prob.item() > 0.75:
         for intent in intents['intents']:
+            if tag == 'goodbye' and tag == intent["tag"]:
+                response = str(random.choice(intent['responses']))
+                rresponse = refactor_response(response)
+                print(f"{bot_name}: {rresponse}")
+                print("\nType a question to continue")
+                choice = input("Press enter to confirm you want to quit: ")
+                if not choice:
+                    exit()
             if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
+                response = str(random.choice(intent['responses']))
+                rresponse = refactor_response(response)
+                print(f"{bot_name}: {rresponse}")
     else:
         print(f"{bot_name}: I am {prob * 100:.2f}% sure that you're talking about '{tag}'. Need more training...")
         falseCounter += 1
